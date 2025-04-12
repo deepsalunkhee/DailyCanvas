4c2ec107379c29b7ea2e02f7347f17037e43d711
deepsalunkhee <deepsalunkhee@gmail.com>
2025-04-12 19:34:27 +0530

    started work on ui:build temp structure of canvas-container

--- dailycanvasClient/src/app/components/canvas-container/canvas-container.component.ts
+++ dailycanvasClient/src/app/components/canvas-container/canvas-container.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { EditorComponent } from "../editor/editor.component";
+import { DisplayerComponent } from "../displayer/displayer.component";
 
 @Component({
   selector: 'app-canvas-container',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, EditorComponent, DisplayerComponent],
   templateUrl: './canvas-container.component.html',
   styleUrl: './canvas-container.component.css'
 })
---
+++ dailycanvasClient/src/app/components/displayer/displayer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayer',
+  imports: [],
+  templateUrl: './displayer.component.html',
+  styleUrl: './displayer.component.css'
+})
+export class DisplayerComponent {
+
+}
